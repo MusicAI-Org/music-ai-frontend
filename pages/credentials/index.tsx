ede1aac7019b25d52a8ce0f1567d0eb8b07f1763
PAARTH2608 <jain.paarth2608@gmail.com>
2023-03-28 00:02:28 +0530

    add:Head-Credentials

--- pages/credentials/index.tsx
+++ pages/credentials/index.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text, useTheme } from "@chakra-ui/react";
+import Head from "next/head";
 import React from "react";
 import ImageCarousal from "../../components/Page-Components/Credentials";
 import {
@@ -21,23 +22,29 @@ const Credential = (): JSX.Element => {
     fontSize: theme.fontSizes.h3,
   };
   return (
-    <PageContainer>
-      <Credentials>
-        <CredentialsContainer>
-          <ImageCarousal />
-          <LoginSignupContainer>
-            <Flex direction="column" height="fit-content" width="90%">
-              <Text style={style}>Have an account?</Text>
-              <LoginButton />
-            </Flex>
-            <Flex direction="column" height="fit-content" width="90%">
-              <Text style={style}>Create a New Account</Text>
-              <SignUpButton />
-            </Flex>
-          </LoginSignupContainer>
-        </CredentialsContainer>
-      </Credentials>
-    </PageContainer>
+    <>
+      <Head>
+        <title>Authenticate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PageContainer>
+        <Credentials>
+          <CredentialsContainer>
+            <ImageCarousal />
+            <LoginSignupContainer>
+              <Flex direction="column" height="fit-content" width="90%">
+                <Text style={style}>Have an account?</Text>
+                <LoginButton />
+              </Flex>
+              <Flex direction="column" height="fit-content" width="90%">
+                <Text style={style}>Create a New Account</Text>
+                <SignUpButton />
+              </Flex>
+            </LoginSignupContainer>
+          </CredentialsContainer>
+        </Credentials>
+      </PageContainer>
+    </>
   );
 };
 export default Credential;
