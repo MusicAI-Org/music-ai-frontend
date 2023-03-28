460fb400ea5259c34d0e99b1e483d1a0fac90ba6
PAARTH2608 <jain.paarth2608@gmail.com>
2023-03-29 00:40:46 +0530

    add:backend-stable-diffusion

--- pages/api/stable-diffusion.ts
+++ pages/api/stable-diffusion.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line require-jsdoc
 export async function stableDiffusionApi(inputValue: any) {
   const response = await fetch(
-    `http://localhost:8000/generate-image/${inputValue}`
+    `https://music-ai-stable-diffusion-api.onrender.com/generate-image/${inputValue}`
   );
   const data = await response.json();
   return data;
